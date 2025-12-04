e563e9f3126109d0507e5946d6b3246b63238dc8
KimtyShrestha <kimtyshrestha@gmail.com>
2025-12-05 01:13:09 +0545

    Added splash screen UI and assets folder

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Navigate to onboarding after 2 seconds
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/onboarding');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(
+          "assets/images/store_logo.png",
+          width: 400,      // Adjust size as needed (200–320 recommended)
+          height: 400,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
