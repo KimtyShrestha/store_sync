53cf05d529b926b839c5dd742f8f62889137c2ab
KimtyShrestha <kimtyshrestha@gmail.com>
2025-12-05 20:08:12 +0545

    Updated main.dart to set onboarding navigation flow

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/splash/splash_screen.dart';
 import 'screens/onboarding/onboarding1_screen.dart';
 import 'screens/onboarding/onboarding2_screen.dart';
+import 'screens/onboarding/onboarding3_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => const SplashScreen(),
         '/onboarding1': (context) => const Onboarding1Screen(),
         '/onboarding2': (context) => const Onboarding2Screen(),
+        '/onboarding3': (context) => const Onboarding3Screen()
 
       },
     );
