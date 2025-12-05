2dbe98b5422687ef782c1548eb522d1c309bef47
KimtyShrestha <kimtyshrestha@gmail.com>
2025-12-05 20:58:01 +0545

    Updated routing and navigation changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'screens/splash/splash_screen.dart';
 import 'screens/onboarding/onboarding1_screen.dart';
 import 'screens/onboarding/onboarding2_screen.dart';
 import 'screens/onboarding/onboarding3_screen.dart';
+import 'screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,8 +25,8 @@ class MyApp extends StatelessWidget {
         '/': (context) => const SplashScreen(),
         '/onboarding1': (context) => const Onboarding1Screen(),
         '/onboarding2': (context) => const Onboarding2Screen(),
-        '/onboarding3': (context) => const Onboarding3Screen()
-
+        '/onboarding3': (context) => const Onboarding3Screen(),
+        '/login': (context) => const LoginScreen(),  // ✅ Added
       },
     );
   }
