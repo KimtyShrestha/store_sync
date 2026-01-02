c91cea97ad113ebdd104f4c6a39ecc5f4b327c25
KimtyShrestha <kimtyshrestha@gmail.com>
2026-01-02 14:38:04 +0545

    Added logout button and cleaned up dashboard navigation

--- lib/features/dashboard/presentation/pages/dashboard_screen.dart
+++ lib/features/dashboard/presentation/pages/dashboard_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../auth/presentation/providers/auth_provider.dart';
 import '../../../home/presentation/pages/home_screen.dart';
 import '../../../records/presentation/pages/records_screen.dart';
 import '../../../projects/presentation/pages/projects_screen.dart';
 import '../../../settings/presentation/pages/settings_screen.dart';
 
-class DashboardScreen extends StatefulWidget {
+class DashboardScreen extends ConsumerStatefulWidget {
   const DashboardScreen({super.key});
 
   @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
+  ConsumerState<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen> {
+class _DashboardScreenState extends ConsumerState<DashboardScreen> {
   int _selectedIndex = 0;
 
   final List<Widget> _screens = const [
@@ -24,17 +27,33 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Store Sync"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              ref.read(authProvider.notifier).logout();
+
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/login',
+                (route) => false,
+              );
+            },
+          ),
+        ],
+      ),
+
       body: _screens[_selectedIndex],
 
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.white,
-
         selectedItemColor: Colors.red,
         unselectedItemColor: Colors.grey,
 
         currentIndex: _selectedIndex,
-
         onTap: (index) {
           setState(() {
             _selectedIndex = index;
