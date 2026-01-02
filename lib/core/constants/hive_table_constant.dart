0c128c7926392803da4f0dddbc1886c9a531e50d
KimtyShrestha <kimtyshrestha@gmail.com>
2026-01-02 14:36:10 +0545

    Added Hive table constant for user storage

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,10 @@
+class HiveTableConstant {
+  // Main DB name
+  static const String dbName = "store_sync_db";
+
+  // Hive type IDs
+  static const int userTypeId = 0;
+
+  // Table names (boxes)
+  static const String userTable = "user_table";
+}
