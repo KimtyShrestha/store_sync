340727d53be87273badb33403b9a6a39e3a8551d
KimtyShrestha <kimtyshrestha@gmail.com>
2026-01-02 14:38:13 +0545

    Added Hive initialization and Riverpod ProviderScope to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:store_sync/features/dashboard/presentation/pages/dashboard_screen.dart';
-import 'package:store_sync/features/home/presentation/pages/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:store_sync/features/auth/presentation/pages/auth_check_page.dart';
+
+import 'app/theme/app_theme.dart';
+
+// Screens
 import 'features/splash/presentation/pages/splash_screen.dart';
 import 'features/onboarding/presentation/pages/onboarding1_screen.dart';
 import 'features/onboarding/presentation/pages/onboarding2_screen.dart';
 import 'features/onboarding/presentation/pages/onboarding3_screen.dart';
 import 'features/auth/presentation/pages/login_screen.dart';
-import 'app/theme/app_theme.dart';
+import 'features/home/presentation/pages/home_screen.dart';
+import 'features/dashboard/presentation/pages/dashboard_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+// Hive Model
+import 'features/auth/data/models/user_model.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserModelAdapter());
+
+  // Riverpod must wrap the App
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,10 +42,9 @@ class MyApp extends StatelessWidget {
 
       theme: getApplicationTheme(),
 
-      // Start from splash screen
       initialRoute: '/',
 
-      // Named routes for navigation
+
       routes: {
         '/': (context) => const SplashScreen(),
         '/onboarding1': (context) => const Onboarding1Screen(),
@@ -34,6 +53,9 @@ class MyApp extends StatelessWidget {
         '/login': (context) => const LoginScreen(),
         '/home': (context) => const HomeScreen(),
         '/dashboard': (context) => const DashboardScreen(),
+        '/auth-check': (context) => const AuthCheckPage(),
+
+
       },
     );
   }
